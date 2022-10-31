cd2474fbc6da23f5babf0224c5c92d7273e99754
Hayyim Del Rio Reynoso <81263578+HayyimDelRioReynoso@users.noreply.github.com>
2022-10-30 21:41:22 -0600

    Add files via upload

---
+++ MensajesAutomaticos.py
@@ -0,0 +1,463 @@
+import pyautogui
+from random import randrange
+import time
+
+'''
+
+@author Hayyim Del Rio Reynoso
+@version 2.0
+
+'''
+
+mensaje1 = "Te amo, feliz cumple,  "
+mensaje2 = "Felicidades, "
+mensaje3 = "Me gustas mucho, "
+mensaje4 = "Gracias por existir, "
+mensaje5 = "Amo ser tu novio, "
+mensaje6 = "Me encantas, "
+mensaje7 = "Me haces feliz, "
+mensaje8 = "Quiero hacerte muy feliz, "
+mensaje9 = "Eres lo mejor que me ha pasado, "
+mensaje10 = "Siempre voy a estar contigo, "
+
+nombre1 = "bonita"
+nombre2 = "preciosa"
+nombre3 = "mi amor"
+nombre4 = "mi vida"
+nombre5 = "hermosa"
+nombre6 = "chiquita"
+nombre7 = "mi cielo"
+nombre8 = "amorcito"
+nombre9 = "nena"
+nombre10 = "bebecita"
+
+time.sleep(25)
+for i in range(1,1440):
+
+    numCond = randrange(10)
+    numMens = randrange(10)
+
+    if numCond == 1:
+        if numMens ==1:
+            pyautogui.typewrite(mensaje1+nombre1)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==2:
+            pyautogui.typewrite(mensaje1+nombre2)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==3:
+            pyautogui.typewrite(mensaje1+nombre3)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==4:
+            pyautogui.typewrite(mensaje1+nombre4)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==5:
+            pyautogui.typewrite(mensaje1+nombre5)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==6:
+            pyautogui.typewrite(mensaje1+nombre6)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==7:
+            pyautogui.typewrite(mensaje1+nombre7)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==8:
+            pyautogui.typewrite(mensaje1+nombre8)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==9:
+            pyautogui.typewrite(mensaje1+nombre9)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==10:
+            pyautogui.typewrite(mensaje1+nombre10)
+            pyautogui.press("enter")
+            time.sleep(60)
+
+    if numCond == 2:
+        if numMens ==1:
+            pyautogui.typewrite(mensaje2+nombre1)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==2:
+            pyautogui.typewrite(mensaje2+nombre2)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==3:
+            pyautogui.typewrite(mensaje2+nombre3)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==4:
+            pyautogui.typewrite(mensaje2+nombre4)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==5:
+            pyautogui.typewrite(mensaje2+nombre5)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==6:
+            pyautogui.typewrite(mensaje2+nombre6)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==7:
+            pyautogui.typewrite(mensaje2+nombre7)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==8:
+            pyautogui.typewrite(mensaje2+nombre8)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==9:
+            pyautogui.typewrite(mensaje2+nombre9)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==10:
+            pyautogui.typewrite(mensaje2+nombre10)
+            pyautogui.press("enter")
+            time.sleep(60)
+
+    if numCond == 3:
+        if numMens ==1:
+            pyautogui.typewrite(mensaje3+nombre1)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==2:
+            pyautogui.typewrite(mensaje3+nombre2)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==3:
+            pyautogui.typewrite(mensaje3+nombre3)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==4:
+            pyautogui.typewrite(mensaje3+nombre4)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==5:
+            pyautogui.typewrite(mensaje3+nombre5)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==6:
+            pyautogui.typewrite(mensaje3+nombre6)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==7:
+            pyautogui.typewrite(mensaje3+nombre7)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==8:
+            pyautogui.typewrite(mensaje3+nombre8)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==9:
+            pyautogui.typewrite(mensaje3+nombre9)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==10:
+            pyautogui.typewrite(mensaje3+nombre10)
+            pyautogui.press("enter")
+            time.sleep(60)
+    
+    if numCond == 4:
+        if numMens ==1:
+            pyautogui.typewrite(mensaje4+nombre1)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==2:
+            pyautogui.typewrite(mensaje4+nombre2)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==3:
+            pyautogui.typewrite(mensaje4+nombre3)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==4:
+            pyautogui.typewrite(mensaje4+nombre4)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==5:
+            pyautogui.typewrite(mensaje4+nombre5)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==6:
+            pyautogui.typewrite(mensaje4+nombre6)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==7:
+            pyautogui.typewrite(mensaje4+nombre7)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==8:
+            pyautogui.typewrite(mensaje4+nombre8)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==9:
+            pyautogui.typewrite(mensaje4+nombre9)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==10:
+            pyautogui.typewrite(mensaje4+nombre10)
+            pyautogui.press("enter")
+            time.sleep(60)
+    
+    if numCond == 5:
+        if numMens ==1:
+            pyautogui.typewrite(mensaje5+nombre1)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==2:
+            pyautogui.typewrite(mensaje5+nombre2)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==3:
+            pyautogui.typewrite(mensaje5+nombre3)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==4:
+            pyautogui.typewrite(mensaje5+nombre4)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==5:
+            pyautogui.typewrite(mensaje5+nombre5)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==6:
+            pyautogui.typewrite(mensaje5+nombre6)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==7:
+            pyautogui.typewrite(mensaje5+nombre7)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==8:
+            pyautogui.typewrite(mensaje5+nombre8)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==9:
+            pyautogui.typewrite(mensaje5+nombre9)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==10:
+            pyautogui.typewrite(mensaje5+nombre10)
+            pyautogui.press("enter")
+            time.sleep(60)
+
+    if numCond == 6:
+        if numMens ==1:
+            pyautogui.typewrite(mensaje6+nombre1)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==2:
+            pyautogui.typewrite(mensaje6+nombre2)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==3:
+            pyautogui.typewrite(mensaje6+nombre3)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==4:
+            pyautogui.typewrite(mensaje6+nombre4)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==5:
+            pyautogui.typewrite(mensaje6+nombre5)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==6:
+            pyautogui.typewrite(mensaje6+nombre6)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==7:
+            pyautogui.typewrite(mensaje6+nombre7)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==8:
+            pyautogui.typewrite(mensaje6+nombre8)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==9:
+            pyautogui.typewrite(mensaje6+nombre9)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==10:
+            pyautogui.typewrite(mensaje6+nombre10)
+            pyautogui.press("enter")
+            time.sleep(60)
+
+    if numCond == 7:
+        if numMens ==1:
+            pyautogui.typewrite(mensaje7+nombre1)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==2:
+            pyautogui.typewrite(mensaje7+nombre2)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==3:
+            pyautogui.typewrite(mensaje7+nombre3)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==4:
+            pyautogui.typewrite(mensaje7+nombre4)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==5:
+            pyautogui.typewrite(mensaje7+nombre5)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==6:
+            pyautogui.typewrite(mensaje7+nombre6)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==7:
+            pyautogui.typewrite(mensaje7+nombre7)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==8:
+            pyautogui.typewrite(mensaje7+nombre8)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==9:
+            pyautogui.typewrite(mensaje7+nombre9)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==10:
+            pyautogui.typewrite(mensaje7+nombre10)
+            pyautogui.press("enter")
+            time.sleep(60)
+
+    if numCond == 8:
+        if numMens ==1:
+            pyautogui.typewrite(mensaje8+nombre1)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==2:
+            pyautogui.typewrite(mensaje8+nombre2)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==3:
+            pyautogui.typewrite(mensaje8+nombre3)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==4:
+            pyautogui.typewrite(mensaje8+nombre4)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==5:
+            pyautogui.typewrite(mensaje8+nombre5)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==6:
+            pyautogui.typewrite(mensaje8+nombre6)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==7:
+            pyautogui.typewrite(mensaje8+nombre7)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==8:
+            pyautogui.typewrite(mensaje8+nombre8)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==9:
+            pyautogui.typewrite(mensaje8+nombre9)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==10:
+            pyautogui.typewrite(mensaje8+nombre10)
+            pyautogui.press("enter")
+            time.sleep(60)
+    
+    if numCond == 9:
+        if numMens ==1:
+            pyautogui.typewrite(mensaje9+nombre1)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==2:
+            pyautogui.typewrite(mensaje9+nombre2)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==3:
+            pyautogui.typewrite(mensaje9+nombre3)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==4:
+            pyautogui.typewrite(mensaje9+nombre4)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==5:
+            pyautogui.typewrite(mensaje9+nombre5)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==6:
+            pyautogui.typewrite(mensaje9+nombre6)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==7:
+            pyautogui.typewrite(mensaje9+nombre7)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==8:
+            pyautogui.typewrite(mensaje9+nombre8)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==9:
+            pyautogui.typewrite(mensaje9+nombre9)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==10:
+            pyautogui.typewrite(mensaje9+nombre10)
+            pyautogui.press("enter")
+            time.sleep(60)
+    
+    if numCond == 10:
+        if numMens ==1:
+            pyautogui.typewrite(mensaje10+nombre1)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==2:
+            pyautogui.typewrite(mensaje10+nombre2)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==3:
+            pyautogui.typewrite(mensaje10+nombre3)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==4:
+            pyautogui.typewrite(mensaje10+nombre4)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==5:
+            pyautogui.typewrite(mensaje10+nombre5)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==6:
+            pyautogui.typewrite(mensaje10+nombre6)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==7:
+            pyautogui.typewrite(mensaje10+nombre7)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==8:
+            pyautogui.typewrite(mensaje10+nombre8)
+            pyautogui.press("enter")
+            time.sleep(60)
+        if numMens ==9:
+            pyautogui.typewrite(mensaje10+nombre9)
+            pyautogui.press("enter")
+            time.sleep(60)
+
+
+        if numMens ==10:
+            pyautogui.typewrite(mensaje10+nombre10)
+            pyautogui.press("enter")
+            time.sleep(60)
+    
+    
+
